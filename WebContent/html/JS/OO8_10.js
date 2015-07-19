41db928f8cb51d87af26f555aaf25faa5850e9d1
whgloveucq <whgloveu@hotmail.com>
2015-07-19 09:59:40 +0800

    another new practice

---
+++ WebContent/html/JS/OO8_10.js
@@ -0,0 +1,51 @@
+Ext.onReady(
+function(){
+Animal=function(config){
+Ext.apply(this.config);
+this.tail="动物的尾巴";
+this.eyes=2;
+}
+Animal.prototype={
+happy:function(){
+alert("摇尾巴");
+},
+eat:function(){
+	alert("动物吃生的");
+},
+run:function(){
+	alert("动物四条腿跑");
+},
+fight:function(){
+	alert("动物 往死里打");
+}
+
+}
+Animal.prototype.constructor=Animal;
+Person=Ext.extend(Animal,{
+tail:null,
+happy:function(){
+alert("微笑");
+},
+eat:function(){
+alert("人是吃熟的");
+},
+run:function(){
+alert("人是两条腿跑");
+},
+fight:Ext.emptyFn
+})
+var p=new Person({name:"大漠穷秋"}) ;
+alert(p.name);
+alert(p.tail);
+alert(p.eyes);
+p.happy();
+p.eat();
+p.run();
+p.fight();
+
+
+}
+
+
+
+);
