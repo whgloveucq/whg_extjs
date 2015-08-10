0dab3158408d11a7d2f89433e36eb67ec2726656
whgloveucq <whgloveu@hotmail.com>
2015-08-10 18:06:19 +0800

    two examples today

---
+++ WebContent/html/JS/UI9_5.js
@@ -0,0 +1,37 @@
+Ext.onReady(
+function(){
+
+var pbar=new Ext.ProgressBar(
+{
+id:'pbar',
+width:300,
+renderTo:Ext.getBody()
+
+}
+
+);
+
+pbar.wait(
+{
+interval:100,
+increment:15,
+duration:5000,
+text:'加载中，请稍候....'
+
+}
+
+);
+
+var btn= new Ext.Button(
+{ text:'停止',
+  handler:function(){
+  pbar.reset();
+  pbar.updateText("强制结束");
+  
+  },
+  renderTo:Ext.getBody()
+
+
+});
+
+});
---
+++ WebContent/html/JS/UI9_7.js
@@ -0,0 +1,31 @@
+Ext.onReady(
+function(){
+
+var pbar=new Ext.ProgressBar(
+{
+id:'pbar',
+width:300,
+renderTo:Ext.getBody()
+
+}
+
+);
+
+var i=0;
+var task={
+run:function(){
+
+	i+=0.1;
+	pbar.updateProgress(i,"加载进度："+Math.round(i*100)+"%");
+	if(Math.round(i*100)==100)
+	{ 
+		pbar.updateText("加载完成");
+		Ext.TaskMgr.stop(task);
+	
+	}
+},
+interval:500
+}
+Ext.TaskMgr.start(task);
+
+});
